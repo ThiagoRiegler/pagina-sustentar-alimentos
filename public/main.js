2a69dc9b07c771cd744ac6d27dcb04c5c6dc4a85
KyloPink <thiagoriegler123@gmail.com>
2022-11-24 00:04:41 -0300

    a

--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
-//window.onscroll = function() {scrollFunction()};
+
 
 function scrollFunction() {
     redes = document.getElementsByClassName("redes");
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar").style.padding = "5px 10px";
-        document.getElementById("navbar-titulo").style.fontSize = "25px";
-        document.getElementById("icon").style.width = "60px";
+        document.getElementById("navbar").style.padding = "10px 10px";
+        document.getElementById("navbar").style.height = "100px";
+        document.getElementById("navbar-titulo").style.fontSize = "0px";
+        document.getElementById("navbar-titulo2").style.fontSize = "15px";
+        document.getElementById("indice").style.top = "-70px";
+        document.getElementById("icon").style.width = "80px";
         for (let index = 0; index < redes.length; index++) {
             
             redes[index].style.width = "35px";
@@ -13,10 +16,13 @@ function scrollFunction() {
         }
         document.getElementById("redes").style.width = "17%";
 
-    } else {
+    } else{
         document.getElementById("navbar").style.padding = "30px 10px";
+        document.getElementById("navbar").style.height = "200px";
         document.getElementById("navbar-titulo").style.fontSize = "35px";
+        document.getElementById("navbar-titulo2").style.fontSize = "0px";
         document.getElementById("icon").style.width = "80px";
+        document.getElementById("indice").style.top = "0px";
         for (let index = 0; index < redes.length; index++) {
             
             redes[index].style.width = "45px";
@@ -26,3 +32,13 @@ function scrollFunction() {
     }
 }
 
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        
+    } else {
+        window.onscroll = function() {scrollFunction()};
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 700px)")
+  myFunction(x) // Call listener function at run time
